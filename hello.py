b997dd855bc65dc9e5bf98b6c900d5a0d6beb38a
Gino <cafetechne@gmail.com>
2019-08-19 23:26:28 -0400

    Familiarizing myself with running python scripts from the terminal, changing files to make those files executable, printing output, declaring variables, and conditionals

---
+++ hello.py
@@ -0,0 +1,6 @@
+import sys
+
+print("The full path of your python interpreter is: ")
+print(sys.executable)
+
+print("This is a hello world example! Hello Python!")
---
+++ ifThens.py
@@ -0,0 +1,21 @@
+
+print("x = 5")
+print("y = 4")
+x=5
+y=4
+
+if x > y :
+  print("x is greater than y!")
+if x < y :
+  print("x is less than y!")
+
+
+print("x = 10")
+print("y = 100")
+x=10
+y=100
+
+if x > y :
+  print("x is greater than y!")
+if x < y :
+  print("x is less than y!")
